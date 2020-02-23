974dac6971322812d1f1cec2e88766ba3e1785f6
unknown <tarun94060sharma@gmail.com>
2020-02-23 22:41:46 +0530

    Functionality of user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import  HttpResponse, Http404, JsonResponse
+from django.http import  HttpResponse, Http404, JsonResponse, HttpResponseRedirect
+from django.contrib.auth.models import User, auth
+from .models import Profile
+from django.urls  import reverse
+from django.utils.crypto import get_random_string
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,9 +21,31 @@ def showRegister(request):
     return render(request, templete, context)
 
 def register(request):
-    print('in register post')
-    if request.POST == 'POST':
 
+    if request.method == 'POST':
+
+        first_name = request.POST.get('first_name')
+        last_name  = request.POST.get('last_name')
+        email      = request.POST.get('email')
+        mobile     = request.POST.get('mobile')
+        password   = request.POST.get('password')
+        username     = get_random_string(length=6, allowed_chars='1234567890')
+        print(first_name)
+
+        print(email)
+        user = User.objects.create_user( first_name=first_name, last_name=last_name, username=username, email=email, password=password )
+        user.profile.mobile = mobile
+        user.save()
+
+        if user:
+            messages.success(request, "Success: Registered successfully.")
+            # messages.success(request, "Success: This is the sample success Flash message.")
+            # messages.error(request, "Error: This is the sample error Flash message.")
+            # messages.info(request, "Info: This is the sample info Flash message.")
+            # messages.warning(request, "Warning: This is the sample warning Flash message.")
+            return HttpResponseRedirect(reverse('register.show'))
+        else:
+            return HttpResponseRedirect(reverse('register.show'))
     else:
         templete = 'accounts/register.html'
         context = {}
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -152,3 +152,16 @@ MEDIA_URL  = '/media/'
 # TEMPLATE_DIRS = (
 #     os.path.join(BASE_DIR, 'templates'),
 # )
+
+
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+except Exception as e:
+    pass
