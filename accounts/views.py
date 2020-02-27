7d5b426d70bcda32fc2c70e70d43f6e528805fc2
unknown <tarun94060sharma@gmail.com>
2020-02-28 01:15:20 +0530

    Added emal confirmation functionality

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,10 @@ from .models import Profile
 from django.urls  import reverse
 from django.utils.crypto import get_random_string
 from django.contrib import messages
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 
@@ -37,6 +41,8 @@ def register(request):
         user.profile.mobile = mobile
         user.save()
 
+        confirm_mail = email_confirmation(user.id,email)
+
         if user:
             messages.success(request, "Success: Registered successfully.")
             # messages.success(request, "Success: This is the sample success Flash message.")
@@ -50,3 +56,33 @@ def register(request):
         templete = 'accounts/register.html'
         context = {}
         return render(request, templete, context)
+
+
+def email_confirmation(user_id, email):
+
+    # Working code without html subject
+
+    # subject    = "E-Com email confirmation"
+    # message    = "For confirmation of your mail click here"
+    # from_email  = "info@ecom.com"
+    # to_email   = [email]
+    # #send_mail('<Your subject>', '<Your message>', 'from@example.com', ['to@example.com'])
+    # send_mail( subject, message, from_email, to_email )
+    # return True
+
+    # With html template
+
+    # Generating 32 digit random string
+    random_string        = get_random_string(length=32)
+    
+
+    # msg = EmailMessage(subject, message, sender, to_list)
+    subject              = "E-Com email confirmation"
+    user_data            = User.objects.get(id=user_id)
+    html_message         = render_to_string('emails/email_confirmation.html', {'user': user_data})
+    plain_message        = strip_tags(html_message)
+    from_email           = "info@ecom.com"
+    to_email             = [email]
+    msg                  = EmailMessage(subject, plain_message, from_email, to_email)
+    msg.content_subtype  = "html"  # Main content is now text/html
+    return msg.send()
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -165,3 +165,10 @@ try:
     }
 except Exception as e:
     pass
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mk49369@gmail.com'
+EMAIL_HOST_PASSWORD = 'eitngszvximswicz'
+EMAIL_PORT = 587
